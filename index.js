47f9d64173e652c3ab325c4ac80730a9b84cf9e2
Pavel <59912686+strikermcs@users.noreply.github.com>
2020-04-29 14:24:32 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const TelegramBot = require('node-telegram-bot-api');
 
-const TOKEN = "1062938753:AAEM81k-Zp7Jt1VUJ4KDIq_PiH0n2VaCUaM";
+const TOKEN = '1062938753:AAEM81k-Zp7Jt1VUJ4KDIq_PiH0n2VaCUaM';
 
 const bot = new TelegramBot(TOKEN, {polling: true});
 
