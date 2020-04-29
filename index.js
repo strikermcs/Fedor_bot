ecdad2f84c0102e377c9f0ca69fc77b1f67a7caf
strikermcs <59912686+strikermcs@users.noreply.github.com>
2020-04-29 13:49:12 +0300

    initial

---
+++ index.js
@@ -0,0 +1,11 @@
+const TelegramBot = require('node-telegram-bot-api');
+
+const TOKEN = "1062938753:AAEM81k-Zp7Jt1VUJ4KDIq_PiH0n2VaCUaM";
+
+const bot = new TelegramBot(TOKEN, {polling: true});
+
+bot.on('message', msg => {
+
+    bot.sendMessage(msg.chat.id, `Привет ${msg.from.first_name}!!! Меня зовут Федор`);
+} )
+
